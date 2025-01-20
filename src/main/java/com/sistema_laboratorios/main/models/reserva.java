c116872e7c7427f3cd4ee99bd033540cf61cdafd
lincolnsanches21 <lincoln.sanches@unifesspa.edu.br>
2025-01-20 20:21:25 -0300

    criação do model reserva

---
+++ src/main/java/com/sistema_laboratorios/main/models/reserva.java
@@ -0,0 +1,76 @@
+package com.sistema_laboratorios.main.models;
+
+import java.sql.Date;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotNull;
+import java.util.Objects;
+import org.apache.commons.lang3.builder.EqualsBuilder;
+
+@Entity
+@Table(name =reserva.TABLE_NAME)
+public class reserva {
+    
+    public static final String TABLE_NAME = "reserva"; 
+
+    @Id
+    @Column(name = "id", unique = true)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column(name = "dataReserva")
+    @NotNull
+    private Date dataReserva;
+    
+
+    public reserva() {
+    }
+
+    public reserva(long id, Date dataReserva) {
+        this.id = id;
+        this.dataReserva = dataReserva;
+    }
+
+    public long getId() {
+        return this.id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public Date getDataReserva() {
+        return this.dataReserva;
+    }
+
+    public void setDataReserva(Date dataReserva) {
+        this.dataReserva = dataReserva;
+    }
+
+    public reserva id(long id) {
+        setId(id);
+        return this;
+    }
+
+    public reserva dataReserva(Date dataReserva) {
+        setDataReserva(dataReserva);
+        return this;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+      return EqualsBuilder.reflectionEquals(this, o);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, dataReserva);
+    }
+
+    
+
+}
